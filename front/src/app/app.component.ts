7e4fb08bc6ac2ede4a8fdab49230a0b133ad0595
baleksab <aleksabozinovic25e@gmail.com>
2024-07-05 14:57:20 +0200

    Added angular material

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements AfterViewInit {
+  title: string = 'front';
+
+  @ViewChild('test') testDiv!: ElementRef;
+
+  ngAfterViewInit(): void {
+    console.log(this.testDiv.nativeElement.innerHTML);
+  }
 }
